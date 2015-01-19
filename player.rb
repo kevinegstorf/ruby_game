c8e5166afb6e36197dec1fee46c8122296c4c63b
kevinegstorf <kevin.egstorf@gmail.com>
2015-01-19 18:47:51 +0100

    added explotion when hit

--- player.rb
+++ player.rb
@@ -2,25 +2,38 @@ class Player
     def initialize(window)
         @window = window
         @icon = Gosu::Image.new(@window, "player.png", true)
-                @x = 0
-                @y = window.height - 80
-        end
-   
-        def move_left
-        @x = @x - 10
-        if @x < 0
-            @x = 0
-        end
+        @x = 0
+        @y = window.height - 80
+        @explosion = Gosu::Image.new(@window, "explosion.png", true)
+        @exploded = false
     end
-        
-        def move_right
+    
+    def move_right
         @x = @x + 10
         if @x > @window.width-50
             @x = @window.width-50
         end
     end
-   
-    def draw
-        @icon.draw(@x,@y,1)
+    
+    def move_left
+        @x = @x - 10
+        if @x < 0
+            @x = 0
+        end
     end
+    
+    def draw
+        if @exploded
+            @explosion.draw(@x, @y, 4)
+        else
+            @icon.draw(@x,@y,1)
+        end
+    end
+    
+    def hit_by?(bullet)
+        if Gosu::distance(bullet.x, bullet.y, @x, @y) < 20
+            @exploded = true
+        end
+    end
+    
 end
--- window.rb
+++ window.rb
@@ -1,25 +1,28 @@
 class Window < Gosu::Window
 
-	def initialize
-		super(300,300,false)
-		@player = Player.new(self)
-		@bullet = Bullet.new(self)
-	end
-
-	def update
-	 	if button_down? Gosu::Button::KbLeft
-	 	@player.move_left
-		end
-
-	 	if button_down? Gosu::Button::KbRight
-	 	@player.move_right
-	 	end
-	 	@bullet.update
-	end
-
-	def draw
-		@player.draw
-		@bullet.draw
-	end
-
+    def initialize
+        super(300,300,false)
+        @player = Player.new(self)
+        @bullet = Bullet.new(self)
+    end
+    
+    def update
+        if @player.hit_by? @bullet
+            #end of game
+        else
+            if button_down? Gosu::Button::KbLeft
+                @player.move_left
+            end
+            if button_down? Gosu::Button::KbRight
+                @player.move_right
+            end
+            @bullet.update
+        end
+    end
+    
+    def draw
+        @player.draw
+        @bullet.draw
+    end
+    
 end
